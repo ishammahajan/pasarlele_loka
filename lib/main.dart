28ed1be32936c181fad1f8169ed5c01f4ac310da
Isham Mahajan <mahajan.isham@gmail.com>
2020-04-26 16:27:37 +0530

    Integrate `home` module, `login`, and `auth_bloc` to `main`.
    Add template dashboard UI module.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/auth_bloc.dart';
+import 'ui/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,34 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String title = 'Pasarlele Loka';
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+
+    return BlocProvider<AuthBloc>(
+      create: (BuildContext context) => AuthBloc(),
+      child: MaterialApp(
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: Home(title: title),
       ),
-      home: MyHomePage(title: title),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Container(),
     );
   }
 }
---
+++ lib/ui/dashboard.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Text('dashboard'),
+    );
+  }
+}
